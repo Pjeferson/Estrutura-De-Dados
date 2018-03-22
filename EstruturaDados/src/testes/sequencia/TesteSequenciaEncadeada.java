e67a4892949d4ed013467b30357145a35cecd1d2
Pjeferson <paulojeferson34@gmail.com>
2018-03-22 18:32:06 -0300

    Adicionando pequeno teste da classe 'sequência'

---
+++ EstruturaDados/src/testes/sequencia/TesteSequenciaEncadeada.java
@@ -0,0 +1,20 @@
+package testes.sequencia;
+
+import estrutura.sequencia.encadeada.SequenciaEncadeada;
+
+public class TesteSequenciaEncadeada {
+	public static void main(String[] args) {
+		SequenciaEncadeada sequencia = new SequenciaEncadeada();
+		
+		sequencia.insertFirst(1);
+		sequencia.insertAtRank(0, 2);
+		int teste = sequencia.size();
+		System.out.println("Tamanho: "+ teste);
+		System.out.println("==========");
+		for (int i = 0; i < teste; i++) {
+			System.out.println(sequencia.removeAtRank(0));
+		}
+		System.out.println("==========");
+		System.out.println("Sequência vazia?: " + sequencia.isEmpty());
+	}
+}
