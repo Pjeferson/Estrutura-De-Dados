8a6929a48242ea25fb193515e4939e5760d54a75
Pjeferson <paulojeferson34@gmail.com>
2018-05-24 23:12:41 -0300

    definindo exceções

---
+++ EstruturaDados/src/estrutura/heap/EHeapCheia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutura.heap;
+
+/**
+ *
+ * @author pjeferson
+ */
+public class EHeapCheia extends RuntimeException{
+
+    public EHeapCheia(String msg) {
+        super(msg);
+    }
+    
+}
---
+++ EstruturaDados/src/estrutura/heap/EHeapVazia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutura.heap;
+
+/**
+ *
+ * @author pjeferson
+ */
+public class EHeapVazia extends RuntimeException{
+
+    public EHeapVazia(String msg) {
+        super(msg);
+    }
+    
+}
