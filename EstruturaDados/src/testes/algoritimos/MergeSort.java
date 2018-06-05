978b07b9346e4964dac09f262d187880685f5b72
Pjeferson <paulojeferson34@gmail.com>
2018-06-04 22:23:04 -0300

    testando ordenação por MergeSort

--- EstruturaDados/src/testes/algoritimos/MergeSort.java
+++ EstruturaDados/src/testes/algoritimos/MergeSort.java
@@ -12,12 +12,13 @@ import estrutura.heap.Celula;
  *
  * @author pjeferson
  */
-public class HeapSort {
+public class MergeSort {
 
     public static void main(String[] args) {
-        Celula[] array = new Celula[10];
+        Celula[] array = new Celula[20];
+        
         for (int i = 0; i < array.length; i++) {
-            int val = (int)Math.floor((Math.random()*10)+1);
+            int val = (int) Math.floor((Math.random() * 10) + 1);
             //int val = i+1;
             Celula celula = new Celula();
             celula.setKey(val);
@@ -26,8 +27,12 @@ public class HeapSort {
             System.out.print(val + " ");
         }
         System.out.print("\n");
+        
+        System.out.println(5/2);
+        
         Ordenacao sort = new Ordenacao();
-        Celula[] arrayOrdenada = sort.heapSort(array);
+        Celula[] arrayOrdenada = sort.mergeSort(array);
+        
         for (int i = 0; i < arrayOrdenada.length; i++) {
             System.out.print(arrayOrdenada[i].getElement() + " ");
         }
